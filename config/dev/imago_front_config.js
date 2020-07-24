94ec51bb03263197f297457776dff0d9a73dbeef
Pierre de Lacroix <pierre@pdelacroix.com>
2020-07-24 21:59:29 +0200

    add imago_front runtime config

---
+++ config/dev/imago_front_config.js
@@ -0,0 +1,5 @@
+// Runtime config
+
+var matrix_url = "http://matrix.imago.local:8008"
+
+var api_url = "http://api.imago.local:4000"
---
+++ config/staging/imago_front_config.js
@@ -0,0 +1,5 @@
+// Runtime config
+
+var matrix_url = "https://matrix.alpha.imago.pm"
+
+var api_url = "https://alpha.imago.pm/api"
